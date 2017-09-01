ab1caa13db7d6e126e771b5b399fe59711c3ebd3
LightningStalker <lgtngstk@hotmail.com>
2017-09-01 07:24:15 -0400

    Add delays for rudimentary switch debouncing

--- Med-Timer.ino
+++ Med-Timer.ino
@@ -36,11 +36,11 @@ void seconds( unsigned long times ) { // same as above for seconds
 
 unsigned long alert() { // slightly modified Blink Without Delay code
   unsigned long previousMillis = 0;
-  unsigned long runTime = 0;         // time since alert began in seconds * 1000 / interval
+  unsigned long runTime = 0;         // time in seconds since alert began
   int alertState = LOW;
 
   while(true) {
-    if (digitalRead(buttonPin) == LOW) { // no need to debounce since timers are seconds - hours
+    if (digitalRead(buttonPin) == LOW) {
       digitalWrite(ledPin, LOW);
       digitalWrite(buzzPin, LOW);
       break;
@@ -49,7 +49,7 @@ unsigned long alert() { // slightly modified Blink Without Delay code
     unsigned long currentMillis = millis();
 
     if (currentMillis - previousMillis >= interval) {
-      // save the last time alert was generated
+      // save the time alert changes state
       previousMillis = currentMillis;
 
       // if the LED/buzzer is off turn it on and vice-versa
@@ -66,6 +66,16 @@ unsigned long alert() { // slightly modified Blink Without Delay code
     }
   }
   return runTime / runTime2m; // return alert runtime in minutes
+}
+
+void beep( bool pause = true ) { // short confirmation beep/flash, pause by default
+  if(pause) seconds(1);          // optional delay in case of switch bounce
+  digitalWrite(ledPin, HIGH);
+  digitalWrite(buzzPin, HIGH);
+  delay(100);
+  digitalWrite(ledPin, LOW);
+  digitalWrite(buzzPin, LOW);
+  return;
 }
 
 // the setup function runs once when you press reset or power the board
@@ -85,24 +95,28 @@ void setup() {
 }
 
 void loop() {
-  unsigned long error = 0; // Accumulated runtime of all alerts
+  unsigned long error = 0;      // accumulated runtime of all alerts
 
-  // initial flash/buzz to let user know we're starting up
-  digitalWrite(ledPin, HIGH);
-  digitalWrite(buzzPin, HIGH);
-  delay(100);
-  digitalWrite(ledPin, LOW);
-  digitalWrite(buzzPin, LOW);
+  // initial flash/buzz to notify user of startup
+  beep(false);                  // no delay since Arduino Micro already has one
 
   // morning dose, begin timing
   hours(4);
-  error = alert(); // 4-hour Medicine
+  error = alert();              // 4-hour Medicine
+  if(error > 60) error = 60;
+  beep();                       // beep confirming start of next timing cycle
+
   minutes(60 - error);
-  error = alert(); // 5-hour Medicine
+  error = alert();              // 5-hour Medicine
+  if(error > 180) error = 180;
+  beep();
+
   minutes(180 - error);
-  error += alert(); // 4-hour Medicine
+  alert();                      // 4-hour Medicine
+  beep();
+
   minutes(240);
-  alert(); // 4-hour Medicine
+  alert();                      // 4-hour Medicine
 
   // time to go to sleep
   noInterrupts();
